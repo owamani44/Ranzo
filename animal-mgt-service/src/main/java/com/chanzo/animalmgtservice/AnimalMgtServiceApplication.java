43a0c1198a8ec7dfdf1aabed3facd93ff3b2cc90
Solomon Owamani <owamani44@gmail.com>
2026-01-09 23:37:22 +0300

    Start animal-mgt service

---
+++ animal-mgt-service/src/main/java/com/chanzo/animalmgtservice/AnimalMgtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chanzo.animalmgtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalMgtServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalMgtServiceApplication.class, args);
+    }
+
+}
---
+++ animal-mgt-service/src/test/java/com/chanzo/animalmgtservice/AnimalMgtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanzo.animalmgtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalMgtServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
